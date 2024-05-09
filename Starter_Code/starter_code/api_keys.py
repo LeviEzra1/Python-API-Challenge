c01c8ffd2b7596902df91bb49a84c1775d068f14
Levi <levi.goold@gmail.com>
2024-05-08 21:58:33 -0700

    Added API keys and started on VacationPy

--- Starter_Code/starter_code/api_keys.py
+++ Starter_Code/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "f699dc299d2e3b09fd01016a14eb4865"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "597533e4759a492b9a1e17e019422c05"
